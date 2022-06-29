33712435274c80e69e3d99b3432455f2badcaf33
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-06-29 20:33:28 +0000

    Product model

--- models/Products.js
+++ models/Products.js
@@ -2,11 +2,20 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const [products] = await connection.execute(
-    'SELECT name FROM StoreManager.products',
+    'SELECT * FROM StoreManager.products',
   );
   return products;
 };
 
+const findById = async (id) => {
+  const [product] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ?',
+    [id],
+  );
+  return product;
+};
+
 module.exports = {
   getAll,
+  findById,
 };
