2a39762ff8ceba68b327c1f0117bde1049c43c0f
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-06-29 20:33:35 +0000

    Product model tests

--- tests/unit/models/products.test.js
+++ tests/unit/models/products.test.js
@@ -2,56 +2,46 @@ const sinon = require("sinon");
 const { expect } = require("chai");
 const { allProducts, singleProduct } = require("../mocks/products");
 const connection = require("../../../models/connection");
-const { getAll } = require('../../../models/Products');
+const { getAll, findById } = require('../../../models/Products');
 
 const error = { message: "Product not found" };
 
 describe("Requesting products with GET", () => {
-  describe("All products", async () => {
+  describe("All products", () => {
+
+    const execute = [allProducts]
+
     beforeEach(async () => {
-      sinon.stub(connection, 'execute').resolves(allProducts);
+      sinon.stub(connection, 'execute').resolves(execute);
     });
 
     afterEach(async () => {
       connection.execute.restore();
     });
 
-    const response = await getAll();
     
-    it("Returns an object", () => {
+    it("Returns an object", async () => {
+      const response = await getAll();
       expect(response).to.equal(allProducts);
-    });
-
-    it("Object contains correct size", () => {
-      expect(response).to.have.length(2);
-    });
-
-    it("Product does not exist", () => {
-      expect(response).to.equal(error);
     });
   });
 
-  describe("Single product", async () => {
+  describe("Single product", () => {
+
+    const execute = [singleProduct]
+
     beforeEach(async () => {
-      sinon.stub(connection, "execute").resolves(singleProduct);
+      sinon.stub(connection, "execute").resolves(execute);
     });
 
     afterEach(async () => {
       connection.execute.restore();
     });
     
-    const response = await Products.findById(1);
     
-    it("Returns an object", () => {
+    it("Returns an object", async () => {
+      const response = await findById(1);
       expect(response).to.equal(singleProduct);
-    });
-    
-    it("Object contains correct size", () => {
-      expect(response).to.have.length(1);
-    });
-    
-    it("Product does not exist", () => {
-      expect(response).to.equal(error);
     });
   });
 });
