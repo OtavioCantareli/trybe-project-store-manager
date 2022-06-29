a67f05ffc9b28694852191872500ddfa6a717db1
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-06-29 19:25:53 +0000

    Attempted test

---
+++ tests/unit/models/products.test.js
@@ -0,0 +1,57 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const { allProducts, singleProduct } = require("../mocks/products");
+const connection = require("../../../models/connection");
+const Products = require('../../../models/Products');
+
+const error = { message: "Product not found" };
+
+describe("Requesting products with GET", () => {
+  describe("All products", async () => {
+    beforeEach(async () => {
+      sinon.stub(connection).resolves(allProducts);
+    });
+
+    afterEach(async () => {
+      connection.execute.restore();
+    });
+
+    const response = await Products.getAll();
+    
+    it("Returns an object", () => {
+      expect(response).to.equal(allProducts);
+    });
+
+    it("Object contains correct size", () => {
+      expect(response).to.have.length(2);
+    });
+
+    it("Product does not exist", () => {
+      expect(response).to.equal(error);
+    });
+  });
+
+  describe("Single product", async () => {
+    // beforeEach(async () => {
+    //   sinon.stub(connection, "execute").resolves(singleProduct);
+    // });
+
+    // afterEach(async () => {
+    //   connection.execute.restore();
+    // });
+    
+    const response = await Products.findById(1);
+    
+    it("Returns an object", () => {
+      expect(response).to.equal(singleProduct);
+    });
+    
+    it("Object contains correct size", () => {
+      expect(response).to.have.length(1);
+    });
+    
+    it("Product does not exist", () => {
+      expect(response).to.equal(error);
+    });
+  });
+});
