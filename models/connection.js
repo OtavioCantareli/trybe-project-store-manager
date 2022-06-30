8c5e4d29229dbc2344356db786b66842cb71fbc5
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-06-30 19:21:56 +0000

    Correct conn

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
+  host: 'store_manager_db',
   user: 'root',
   password: 'password',
   database: 'StoreManager',
