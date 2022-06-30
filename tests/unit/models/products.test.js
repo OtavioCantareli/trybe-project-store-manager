a741dc784d1b513de38e54b3812d1ac0ede086d3
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-06-30 16:19:18 +0000

    Fixed test cases

--- tests/unit/models/products.test.js
+++ tests/unit/models/products.test.js
@@ -3,43 +3,49 @@ const { expect } = require("chai");
 const { allProducts, singleProduct } = require("../mocks/products");
 const connection = require("../../../models/connection");
 const { getAll, findById } = require('../../../models/Products');
+const error = require('../../../middlewares/error');
 
 describe("MODEL", () => {
   describe("All products", () => {
 
-    const execute = [allProducts]
-
-    beforeEach(async () => {
-      sinon.stub(connection, 'execute').resolves(execute);
-    });
-
     afterEach(async () => {
       connection.execute.restore();
     });
 
     
     it("Returns an object", async () => {
+      const execute = [allProducts]
+      sinon.stub(connection, 'execute').resolves(execute);
       const response = await getAll();
       expect(response).to.equal(allProducts);
     });
+
+    it("Returns error if empty", async () => {
+      const execute = []
+      sinon.stub(connection, 'execute').resolves(execute);
+      const response = await getAll();
+      expect(response).to.equal(error);
+    })
   });
 
   describe("Single product", () => {
 
-    const execute = [singleProduct]
-
-    beforeEach(async () => {
-      sinon.stub(connection, "execute").resolves(execute);
-    });
-
     afterEach(async () => {
       connection.execute.restore();
     });
     
-    
     it("Returns an object", async () => {
+      const execute = [singleProduct]
+      sinon.stub(connection, "execute").resolves(execute);
       const response = await findById(1);
       expect(response).to.equal(singleProduct);
     });
+
+    it("Returns error if empty", async () => {
+      const execute = []
+      sinon.stub(connection, 'execute').resolves(execute);
+      const response = await findById();
+      expect(response).to.equal(error);
+    })
   });
 });
