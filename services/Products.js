fbd3463903a752dbba65b89609d95b0e5915c0f0
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-06-30 17:27:49 +0000

    Remove return

--- services/Products.js
+++ services/Products.js
@@ -1,15 +1,12 @@
 const Products = require('../models/Products');
-const error = require('../middlewares/error');
 
 const getAll = async () => {
   const object = await Products.getAll();
-  if (object.length < 1) return error;
   return object;
 };
 
 const findById = async (id) => {
   const object = await Products.findById(id);
-  if (object.length < 1) return error;
   return object;
 };
 
