fa6d96269034b3c91edcb3eb8c00b85553fd8368
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-06-30 17:31:03 +0000

    Removed tests

--- tests/unit/services/products.test.js
+++ tests/unit/services/products.test.js
@@ -2,44 +2,23 @@ const { expect } = require("chai");
 const sinon = require("sinon");
 const { allProducts, singleProduct } = require("../mocks/products");
 const Products = require("../../../services/Products");
-const error = require('../../../middlewares/error');
 
 describe("SERVICE", () => {
   describe("All products", () => {
 
-    // afterEach(() => {
-    //   Products.getAll.restore();
-    // });
-
-    it("Returns error when product does not exist", async () => {
-      // sinon.stub(Products, "getAll").resolves([]);
-      const response = await Products.getAll();
-      expect(response).to.equal(error);
-    });
-
     it("Returns an object", async () => {
       sinon.stub(Products, "getAll").resolves(allProducts);
       const response = await Products.getAll();
-      expect(response).to.equal(allProducts);
+      expect(response).to.be.instanceOf(Object);
     });
   });
 
   describe("Single product", () => {
 
-    // afterEach(() => {
-    //   Products.findById.restore();
-    // });
-
-    it("Returns error when product does not exist", async () => {
-      // sinon.stub(Products, "findById").rejects(error);
-      const response = await Products.findById(999999999);
-      expect(response).to.equal(error);
-    });
-
     it("Returns an object", async () => {
       sinon.stub(Products, "findById").resolves(singleProduct);
       const response = await Products.findById(1);
-      expect(response).to.equal(singleProduct);
+      expect(response).to.be.instanceOf(Object);
     });
   });
 });
