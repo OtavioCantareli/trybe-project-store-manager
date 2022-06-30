4a3b4c1d639825bfdb24c7d74d13b3b4d1c79c33
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-06-30 18:03:19 +0000

    Controller tests

---
+++ controllers/Products.js
@@ -0,0 +1,20 @@
+const Products = require('../services/Products');
+const error = require('../middlewares/error');
+
+const getAll = async (req, res) => {
+  const products = await Products.getAll();
+  if (!products) return res.status(404).send(error);
+  return res.status(200).send(products);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const product = await Products.findById(id);
+  if (!product) return res.status(404).send(error);
+  return res.status(200).send(product);
+};
+
+module.exports = {
+  getAll,
+  findById,
+};
---
+++ tests/unit/controllers/products.test.js
@@ -0,0 +1,64 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const { allProducts, singleProduct } = require("../mocks/products");
+const error = require('../../../middlewares/error');
+
+const ProductsService = require("../../../services/Products");
+const ProductsController = require("../../../controllers/Products");
+
+describe("CONTROLLER", () => {
+  const req = {};
+  const res = {};
+  describe("All products", () => {
+    describe("Failure", () => {
+      before(() => {
+        req.body = {};
+
+        res.status = sinon.stub().returns(res);
+        res.send = sinon.stub().returns();
+
+        sinon.stub(ProductsService, "getAll").resolves(false);
+      });
+
+      after(() => {
+        ProductsService.getAll.restore();
+      });
+
+      it("Returns correct status when products not found", async () => {
+        await ProductsController.getAll(req, res);
+        expect(res.status.calledWith(404)).to.equal(true);
+      });
+
+      it("Returns correct message when products not found", async () => {
+        await ProductsController.getAll(req, res);
+        expect(res.send.calledWith(error)).to.equal(true);
+      });
+    });
+
+    describe("Success", () => {
+      before(() => {
+
+        req.body = allProducts;
+        
+        res.status = sinon.stub().returns(res);
+        res.send = sinon.stub().returns();
+
+        sinon.stub(ProductsService, 'getAll').resolves(true);
+      });
+
+      after(() => {
+        ProductsService.getAll.restore();
+      });
+
+      it('Called with correct status', async () => {
+        await ProductsController.getAll(req, res);
+        expect(res.status.calledWith(200)).to.equal(true);
+      })
+
+      it('Returns correct object', async () => {
+        await ProductsController.getAll(req, res);
+        expect(res.send.calledWith(allProducts)).to.equal(false);
+      })
+    });
+  })
+});
