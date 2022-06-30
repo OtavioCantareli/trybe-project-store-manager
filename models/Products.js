77183e3478a91caf83d51d33719b82f5c17eeb5f
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-06-30 16:58:32 +0000

    removed return error

--- models/Products.js
+++ models/Products.js
@@ -1,11 +1,9 @@
 const connection = require('./connection');
-const error = require('../middlewares/error');
 
 const getAll = async () => {
   const [products] = await connection.execute(
     'SELECT * FROM StoreManager.products',
   );
-  if (!products) return error;
   return products;
 };
 
@@ -14,7 +12,6 @@ const findById = async (id) => {
     'SELECT * FROM StoreManager.products WHERE id = ?',
     [id],
   );
-  if (!product) return error;
   return product;
 };
 
