378da7f1d5ac2069214305a09cbd56221a676be6
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-07-01 20:01:57 +0000

    Added middleware to route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const ProductsController = require('./controllers/Products');
+const validateName = require('./middlewares/validateName');
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,7 +16,7 @@ app.get('/products', (req, res) => ProductsController.getAll(req, res));
 
 app.get('/products/:id', (req, res) => ProductsController.findById(req, res));
 
-app.post('/products', (req, res) => ProductsController.insert(req, res));
+app.post('/products', validateName, (req, res) => ProductsController.insert(req, res));
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
