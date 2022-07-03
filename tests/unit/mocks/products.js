321bc5745af63e907d472f11a02129a10753221c
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-07-03 15:44:59 +0000

    mocks

--- tests/unit/mocks/products.js
+++ tests/unit/mocks/products.js
@@ -23,9 +23,36 @@ const resultInsertProduct = {
   name: "ProdutoX",
 };
 
+const insertSales = [
+  {
+    productId: 1,
+    quantity: 1,
+  },
+  {
+    productId: 2,
+    quantity: 5,
+  },
+];
+
+const resultInsertSales = {
+  id: 3,
+  itemsSold: [
+    {
+      productId: 1,
+      quantity: 1,
+    },
+    {
+      productId: 2,
+      quantity: 5,
+    },
+  ],
+};
+
 module.exports = {
   allProducts,
   singleProduct,
   insertProduct,
   resultInsertProduct,
+  insertSales,
+  resultInsertSales,
 };
